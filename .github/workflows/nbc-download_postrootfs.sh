17af37512f508f17927b3876daa7ab1c0670d869
Brian Ketelsen <bketelsen@gmail.com>
2025-12-24 20:49:40 -0500

    feat: offline and online isos
    Signed-off-by: Brian Ketelsen <bketelsen@gmail.com>

---
+++ .github/workflows/nbc-download_postrootfs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+# check to see if `nbc` command is available
+if ! command -v nbc &> /dev/null
+then
+    echo "nbc could not be found, please install nbc to proceed."
+    exit 1
+fi
+
+# download the installation image for offline use
+nbc download --image ghcr.io/frostyard/snow:latest --for-install
